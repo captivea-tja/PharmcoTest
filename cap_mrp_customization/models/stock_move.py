2511a8b65ed5703b140de7e6924f3e6cec8c4512
captivea-bta <captivea-bta>
2020-06-26 18:15:08 +0530

    [IMP] Updated code to assign multiple lot / serial number details on transfer

--- cap_mrp_customization/models/stock_move.py
+++ cap_mrp_customization/models/stock_move.py
@@ -6,6 +6,19 @@ from odoo import api, models, fields, _
 class StockMove(models.Model):
     _inherit = 'stock.move'
 
+    manufacturer_lot = fields.Char(string="Manufacturer's Lot")
+    expiration_date = fields.Date(string="Expiration Date")
+    tare_weight = fields.Float(string="Tare Weight")
+    gross_weight = fields.Float(string="Gross Weight")
+    container_type = fields.Char(string="Container Type")
+    manufacture_date = fields.Date(string="Date of Manufacture")
+
+    def generate_sequence_number(self):
+        sequence_ref = self.env.ref('stock.sequence_production_lots')
+        if sequence_ref and sequence_ref.number_next_actual:
+            next_seq = sequence_ref.get_next_char(sequence_ref.number_next_actual)
+            self.next_serial = next_seq
+
     def action_show_details(self):
         res = super(StockMove, self).action_show_details()
         res.update({'target': self})
--- cap_mrp_customization/models/stock_move_line.py
+++ cap_mrp_customization/models/stock_move_line.py
@@ -29,3 +29,22 @@ class StockMoveLine(models.Model):
             'company_id': self.company_id.id
         })
 
+    @api.model
+    def create(self, vals):
+        if vals.get('move_id'):
+            move_id = self.env['stock.move'].browse(vals.get('move_id'))
+            vals.update({
+                'manufacturer_lot': move_id.manufacturer_lot,
+                'expiration_date': move_id.expiration_date,
+                'tare_weight': move_id.tare_weight,
+                'gross_weight': move_id.gross_weight,
+                'container_type': move_id.container_type,
+                'manufacture_date': move_id.manufacture_date
+            })
+        if not vals.get('lot_id', False) and vals.get('lot_name', False):
+            lot_id = self.env['stock.production.lot'].create({
+                'product_id': vals.get('product_id'),
+                'company_id': vals.get('company_id') or self.env.user.company_id.id
+            })
+            vals.update({'lot_id': lot_id.id})
+        return super(StockMoveLine, self).create(vals)
