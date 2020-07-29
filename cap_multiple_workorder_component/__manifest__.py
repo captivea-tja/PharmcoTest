7015d092e122e27dacfb84c32d46737e96ff2852
captivea-bta <captivea-bta>
2020-07-29 16:13:52 +0530

    [IMP] Updated assign serial number and fixed fresh insatallation issue

--- cap_multiple_workorder_component/__manifest__.py
+++ cap_multiple_workorder_component/__manifest__.py
@@ -14,7 +14,7 @@
     'category': 'Manufacturing/Manufacturing',
 
     # any module necessary for this one to work correctly
-    'depends': ['mrp_workorder'],
+    'depends': ['mrp_workorder', 'quality_control'],
 
     # always loaded
     'data': [
